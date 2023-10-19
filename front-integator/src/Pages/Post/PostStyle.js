4cfbad1c5bab99c35a1dcbf4d428bbaf216d82df
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2023-10-19 16:29:50 -0300

    Editando o post

--- front-integator/src/Pages/Post/PostStyle.js
+++ front-integator/src/Pages/Post/PostStyle.js
@@ -1,12 +1,33 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+    width: 100vw;
+    padding: 3.5vh 5vw 3.5vh 5vw;
 `
 export const FormPost = styled.form`
+    display: flex;
+    flex-direction: column;
+    height: 21vh;
 `
-export const InputContent = styled.input.attrs({type: "text", autoComplete: "off", id: "content", name: "content"})`
+
+export const InputContent = styled.textarea.attrs({autoComplete: "off", id: "content", name: "content"})`
+    height: 15vh;
+    vertical-align: top;
+    padding-left: 4vw;
+    padding-top: 5vw;
+    padding-right: 4vw;
+    resize: none;
+    border-radius: 1em;
+    border: none;
+    background-color: #EDEDED;
 `
 export const ButtonPost = styled.input.attrs({type: "submit"})`
+    margin-top: 2vh;
+    height: 4vh;
+    background: linear-gradient(to right, #FF6489, #F9B24E );
+    border: none;
+    border-radius: 0.7em;
+
 `
 export const ListPost = styled.ul`
 `
--- front-integator/src/components/Header/HeaderStyle.js
+++ front-integator/src/components/Header/HeaderStyle.js
@@ -3,17 +3,19 @@ import { TbX } from "react-icons/tb";
 
 const stayle = {
     post: {
-        width: "55vw", 
+        width: "50vw", 
         justifyContent: "flex-end"
     }
 }
 
 export const HeaderComponent = styled.header`
     width: 100vw;
+    height: 6vh;
     display: flex;
     padding-right: 10vw;
     padding-left: 10vw;
     justify-content: ${props => props.$post ? stayle.post.justifyContent : "center"};
+    background-color: #EDEDED;
 `
 
 export const NavHeaderPost = styled.nav`
@@ -25,22 +27,31 @@ export const NavHeaderPost = styled.nav`
 
 
 export const MiniLogo = styled.img`
-    height: 8vh;
+    height: 4vh;
 
 `
 
 export const ButtonEnter = styled.button`
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    color: #4088CB;
+    font-weight: 600;
+    font-size: 2cqmax;
 `
 export const ButtonLogout = styled.button`
     background-color: transparent;
     border: none;
     cursor: pointer;
+    color: #4088CB;
+    font-weight: 600;
+    font-size: 2vh;
 `
 export const Close = (props) => {
 
     const closePage = props.closePage
 
     return(
-        <TbX onClick={closePage}/>
+        <TbX onClick={closePage} style={{fontSize: "4vh"}}/>
     )
 }
