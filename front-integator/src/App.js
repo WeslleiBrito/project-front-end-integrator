433a87e0d88d0e6e52a833c7f806de24f99ea468
WeslleiBrito <wesllei.wbs@gmail.com>
2023-10-27 20:19:32 -0300

    Inclusão do chakra ui ao projeto

--- front-integator/src/App.js
+++ front-integator/src/App.js
@@ -1,15 +1,16 @@
 import { Router } from './Routes/Router';
 import { GlobalState } from './context/GlobalState';
 import { GlobalStyle } from './globalStyled';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <GlobalStyle/>
       <GlobalState>
         <Router/>
       </GlobalState>
-    </>
+    </ChakraProvider>
   );
 }
 
--- front-integator/src/Pages/Signup/SignupPage.js
+++ front-integator/src/Pages/Signup/SignupPage.js
@@ -35,7 +35,6 @@ export const SignupPage = () => {
 
             if(error instanceof AxiosError){
                 alert(error.response.data)
-                
             }else{
                 alert(error)
             }
